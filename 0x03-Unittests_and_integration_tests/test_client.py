dbca7e56881581c60e8743dbc567e4427a7b83cd
Calebkillom <Calebkillom@gmail.com>
2024-09-10 22:55:47 +0300

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Teus Sep 10 22:52:17 2024
+Created on Teus Sep 10 21:57:00 2024
 
 @Author: Caleb Kilonzi
 """
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -30,3 +30,16 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_url = "https://api.github.com/orgs/" + org_name
         mock_get_json.assert_called_once_with(expected_url)
         self.assertEqual(result, mock_response)
+
+    @parameterized.expand([
+        ("google", "https://api.github.com/orgs/google/repos"),
+        ("abc", "https://api.github.com/orgs/abc/repos"),
+    ])
+    def test_public_repos_url(self, org_name, expected_url):
+        """Test that _public_repos_url returns the correct URL based on org."""
+        with patch.object(GithubOrgClient, 'org', \
+						  new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": expected_url}
+            client = GithubOrgClient(org_name)
+            result = client._public_repos_url
+            self.assertEqual(result, expected_url)
